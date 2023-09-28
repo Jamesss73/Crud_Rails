2accc13acbc1c4c3d49a7282d4602254bf5d8d30
jamesjms20 <jamessoscue20@gmail.com>
2023-09-28 00:47:01 -0500

    add initial
    se agrega base de desarrollo del crud en rais

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,48 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+      if @product.save
+        redirect_to products_path
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to products_path
+      else
+        render :edit
+      end
+  end
+
+  def show  ###  es el metodo Delete pero por alguna razon  no logro que se use correctamente  
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to products_path, notice: "Producto eliminado correctamente"
+  end
+
+  def delete
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to products_path, notice: "Producto eliminado correctamente"
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+    end
+end
